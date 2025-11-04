0b134989be182c1af0ba929f6add5ea67d4a428f
DevStackJunior <pv00wyv@eduvaud.ch>
2025-11-04 09:59:39 +0100

    Fix: Restructuring Files/Architecture

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Home from './pages/Home.vue'
 </script>
 
 <template>
