6aa857ed80907854e86fd78678eecd68e9c69123
Meronada749 <meron.essayas@eduvaud.ch>
2025-11-11 10:35:31 +0100

    initial commit
    Co-Authored-By: Chris0xdeadbeef <195429498+Chris0xdeadbeef@users.noreply.github.com>

---
+++ src/components/Background.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps<{
+  background?: string // couleur / gradient / image
+  height?: string // hauteur de la bande
+  position?: 'top' | 'bottom' // emplacement de la bande
+}>()
+</script>
+
+<template>
+  <div
+    class="background-band"
+    :class="[position]"
+    :style="{
+      '--band-bg': background || '#f3f3f3',
+      '--band-height': height || '120px',
+    }"
+  ></div>
+</template>
+
+<style scoped>
+.background-band {
+  width: 100%;
+  height: var(--band-height);
+  background: var(--band-bg);
+  background-size: cover; /* ajuste l'image à la bande */
+  background-position: center; /* centre l'image */
+  background-repeat: no-repeat; /* pas de répétition par défaut */
+  position: absolute;
+  left: 0;
+  right: 0;
+  z-index: 1;
+}
+
+/* Position haute */
+.background-band.top {
+  top: 0;
+}
+
+/* Position basse */
+.background-band.bottom {
+  bottom: 0;
+}
+</style>
---
+++ src/components/VisitedInfo.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+defineProps<{
+  visitedDate: string
+  show?: boolean
+}>()
+</script>
+
+<template>
+  <div v-if="show" class="visited-container-wrapper">
+    <div class="visited-container">
+      <div class="visited-left">
+        <div class="visited-header">
+          <h2 class="visited-title">Visité</h2>
+        </div>
+        <p class="visited-date">{{ visitedDate }}</p>
+      </div>
+
+      <div class="visited-right">
+        <img
+          src="../assets/img/visited-check.png"
+          alt="Icône de vérification"
+          class="visited-icon"
+        />
+      </div>
+    </div>
+  </div>
+
+  <!-- Bloc partage (affiché seulement si showShare est true) -->
+  <div v-if="show" class="share-block">
+    <p>Voulez-vous le partager avec vos amis ?</p>
+  </div>
+</template>
+
+<style scoped>
+.visited-container-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end; /* pousse le contenu en bas */
+  min-height: 60vh; /* prend toute la hauteur de la page */
+}
+.visited-container {
+  display: flex;
+  gap: 1rem;
+  font-family: 'Poppins', sans-serif;
+  justify-content: center;
+  align-items: stretch;
+}
+.visited-left {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.visited-title {
+  font-size: 1.4rem;
+  font-weight: 600;
+  margin: 0;
+}
+
+.visited-date {
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: #4b4b4b;
+}
+
+.visited-right {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.visited-icon {
+  width: 90px; /* ajuste selon ton besoin */
+  height: 100%;
+  object-fit: contain; /* pour que l'image garde ses proportions */
+}
+
+.share-block {
+  text-align: center;
+  font-weight: 400;
+}
+</style>
