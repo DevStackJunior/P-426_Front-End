9729aead4608ae23f8b90471da7a37d046c2ef9f
Meronada749 <meron.essayas@eduvaud.ch>
2025-11-11 10:37:08 +0100

    initial commit
    Co-Authored-By: Nelson Almeida <195763919+NelsonAlmeida5@users.noreply.github.com>

---
+++ src/components/SiteDecor.vue
@@ -0,0 +1,44 @@
+<script setup></script>
+
+<template>
+  <div class="decor">
+    <img
+      src="../assets/img/left-corner-img.png"
+      class="deco-left"
+      alt="Décoration gauche"
+    />
+    <img
+      src="../assets/img/right-corner-img.png"
+      class="deco-right"
+      alt="Décoration droite"
+    />
+  </div>
+</template>
+
+<style scoped>
+.decor {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  z-index: 0;
+}
+
+.deco-left,
+.deco-right {
+  position: absolute;
+  bottom: 0;
+  opacity: 0.25;
+  width: 460px;
+}
+
+.deco-left {
+  left: 0;
+}
+
+.deco-right {
+  right: 0;
+}
+</style>
